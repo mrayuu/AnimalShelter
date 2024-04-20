f92ac43b259b0f1f3b1d2e276f202840751e994a
mrayuu <merzlovaanastasia@gmail.com>
2024-04-20 22:50:43 +0300

    create tests

---
+++ AnimalShelter/src/test/java/com/example/AnimalShelter/tests/AnimalControllerTest.java
@@ -0,0 +1,57 @@
+package com.example.AnimalShelter.tests;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import com.example.AnimalShelter.entity.AnimalEntity;
+import com.example.AnimalShelter.repository.AnimalRepo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AnimalControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private AnimalRepo animalRepo;
+
+    @Test
+    public void testAnimalAdd() throws Exception {
+        mockMvc.perform(post("/animals/add")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .param("name", "TestAnimal")
+                        .param("gender", "Male")
+                        .param("age", "3")
+                        .param("kind", "Dog")
+                        .param("breed", "Labrador")
+                        .param("color", "Brown")
+                        .param("size", "Large")
+                        .param("vaccinations", "Yes")
+                        .param("diseases", "None")
+                        .param("description", "Friendly and playful")
+                        .with(user("admin").password("admin").roles("ADMIN"))
+                )
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login"));
+
+        // Проверяем, что животное успешно добавлено в базу данных
+        Iterable<AnimalEntity> animals = animalRepo.findAll();
+        boolean animalAdded = false;
+        for (AnimalEntity animal : animals) {
+            if (animal.getName().equals("TestAnimal")) {
+                animalAdded = true;
+                break;
+            }
+        }
+        assert (animalAdded);
+    }
+}
